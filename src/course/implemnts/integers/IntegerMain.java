bf852bf7ba14de73907e03bc4b19d5464ead7e31
Nagenderbabu <nagenderbabu34@gmail.com>
2017-03-24 14:19:17 +0530

    Added comments

--- src/course/implemnts/integers/IntegerMain.java
+++ src/course/implemnts/integers/IntegerMain.java
@@ -8,13 +8,19 @@ public class IntegerMain extends PrintInteger{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		// initializing implemented(PrintInteger) class and referring it to interface class object
 		IntegerDemo id =new PrintInteger();
+		// initializing implemented(SquareInteger) class and referring it to interface class object
 		IntegerDemo id1=new SquareInteger();
+		// Scanner object to take input from user
 	   Scanner sc=new Scanner(System.in);
+	   // display statement for user asking entry of input
 		System.out.println("enter any integer value :");
-	   int value=sc.nextInt();
-	   id.setInteger(value);
+	   // passing input from scanner object to local variable
+		int value=sc.nextInt();
+	   // displays input value
+		id.setInteger(value);
+	   // displays square of given input value
 	   id1.setInteger(value);
 		
 	}
